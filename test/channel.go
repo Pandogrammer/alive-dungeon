54d04047232ce113e34d38faee72119566981d50
Pandogrammer <pandogrammer@gmail.com>
2020-01-23 22:18:45 -0300

    Input del usuario

--- test/channel.go
+++ test/channel.go
@@ -1,19 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"testing"
+	"os"
 	"time"
 )
-
-func TestChannel(t *testing.T) {
-	messages := make(chan string)
-
-	go func() { messages <- "ping" }()
-
-	msg := <-messages
-	fmt.Println(msg)
-}
 
 type Action int
 
@@ -24,38 +16,49 @@ const (
 
 func main() {
 	actions := make(chan Action, 1)
-	changes := make(chan int, 1)
+	state := make(chan int, 1)
 	initialState := 0
 
-	process(initialState, actions, changes)
-	sender(actions)
-	receiver(changes)
+	go process(initialState, actions, state)
+	go sender(actions)
+	go input(actions)
+
+	receiver(state)
 }
 
-func receiver(changes chan int) {
+func input(actions chan Action) {
+	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Println(<-changes)
+		message, _ := reader.ReadString('\n')
+		if message == "x\n" {
+			actions <- Decrement
+		}
+	}
+}
+
+func receiver(state chan int) {
+	for {
+		fmt.Println(<-state)
 	}
 }
 
 func sender(actions chan Action) {
-	go func() {
-		for {
-			actions <- Increment
-			time.Sleep(time.Second)
-		}
-	}()
+	for {
+		actions <- Increment
+		time.Sleep(time.Second)
+	}
 }
 
-func process(initialState int, actions <-chan Action, changes chan<- int) {
-	go func() {
-		var state = initialState
-		for {
-			switch <-actions {
-			case Increment:
-				state = state + 1
-				changes <- state
-			}
+func process(initialState int, actions <-chan Action, state chan<- int) {
+	var actualState = initialState
+	for {
+		switch <-actions {
+		case Increment:
+			actualState = actualState + 1
+			state <- actualState
+		case Decrement:
+			actualState = actualState - 1
+			state <- actualState
 		}
-	}()
+	}
 }
